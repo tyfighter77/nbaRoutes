d5c316de9d68c6ed882ac71d0e965bf774a875c0
Ty Hill <crazysnoheadman@yahoo.com>
2016-01-04 16:58:03 -0700

    completed assignment

--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -1,5 +1,9 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('homeCtrl', function ($scope, homeService) {
+app.controller('homeCtrl', function ($scope, homeService, jazzData, lakersData, heatData) {
+
+$scope.jazzData = jazzData;
+$scope.lakersData = lakersData;
+$scope.heatData = heatData;
 
 });
